be7cfa2b10637ab9fe46df288c1cfe878294df02
Travis Purcell <travis.n.purcell@gmail.com>
2026-03-01 17:36:39 -0700

    Replace individual Routes with apiResource

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,7 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/notes', [NoteController::class, 'index']);
-Route::post('/notes', [NoteController::class, 'store']);
-Route::put('/notes/{note}', [NoteController::class, 'update']);
-Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
+// Route::get('/notes', [NoteController::class, 'index']);
+// Route::post('/notes', [NoteController::class, 'store']);
+// Route::put('/notes/{note}', [NoteController::class, 'update']);
+// Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
+
+Route::apiResource('notes', NoteController::class);
